f063d3075fc945e533cacce805e9ec4fb25ae5fc
Frode Danielsen <frode@e5r.no>
2013-01-26 23:04:25 +0100

    Initial setup of blog project with pelican

---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Frode Danielsen'
+SITENAME = u'head-spinoff'
+SITEURL = 'http://frode.danielsen.net/blog'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('Thore, my dad', 'http://thore.no'),
+          ('Ruben, my brother', 'http://rubendanielsen.com'),
+          ('Stian, my colleague', 'http://blogg.prestholdt.no'),
+          ('Eliksir, my company', 'http://e5r.no'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
